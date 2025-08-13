9f809d53b2af0f2e93cf5d7d0de38c7e902749ec
Nicolas José Coninck <nicolas.coninck@remotatec.com.br>
2025-08-13 09:27:18 -0300

    Alterações seguindo o curso, Implementadas rotinas de view, manipulação do DOM a partir do JavaScript

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,37 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
+export class NegociacoesView {
+
+    private element: HTMLElement;
+    
+    constructor(selector: string) {
+        this.element = document.querySelector(selector)
+    }
+
+    template(model: Negociacoes): string {
+        return `<table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${model.lista().map(negociacao => {
+                            return `<tr>
+                                        <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                        <td>${negociacao.quantidade}</td>
+                                        <td>${negociacao.valor}</td>
+                                    </tr>`;
+                        }).join('')}
+                    </tbody>
+                </table>`;
+    }
+
+    updateView(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template);
+        this.element.innerHTML = template;
+    }
+}
